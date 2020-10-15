e6c963c241254c3d3be14ffe194d82d285670396
Shika <ahmedkhaled19701@gmail.com>
2020-10-15 17:05:38 +0200

    refactoring

--- ScrapingDisney.py
+++ ScrapingDisney.py
@@ -3,20 +3,38 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-r = requests.get('https://en.wikipedia.org/wiki/Toy_Story_3')
+def main():
+    r = requests.get('https://en.wikipedia.org/wiki/Toy_Story_3')
+    soup = bs(r.content)
+    # contents = soup.prettify()
+    # print(contents)
 
-soup = bs(r.content)
-# contents = soup.prettify()
-# print(contents)
+    info_box = soup.find(class_="infobox vevent")
+    # print(info_box.prettify())
 
-info_box = soup.find(class_="infobox vevent")
-# print(info_box.prettify())
+    info_rows = info_box.find_all("tr")
+    # for row in info_box:
+    #     print(row.prettify())
 
-info_rows = info_box.find_all("tr")
-# for row in info_box:
-#     print(row.prettify())
+    # print(len(info_rows))
 
-# print(len(info_rows))
+    movies_info = get_movie_data(info_box, info_rows)
+    print(movies_info)
+
+
+def get_movie_data(info_box, info_rows):
+    movie_info = {}
+    for index, row in enumerate(info_rows):
+        if index == 0:
+            movie_info['Title'] = row.find('th').get_text(" ", strip=True)
+        elif index == 1:
+            continue
+        else:
+            content_key = row.find('th').get_text(" ", strip=True)
+            content_value = get_content_value(row.find('td'))
+            movie_info[content_key] = content_value
+
+    return movie_info
 
 
 def get_content_value(row_data):
@@ -26,15 +44,5 @@ def get_content_value(row_data):
         return row_data.get_text(" ", strip=True).replace('\xa0', ' ')
 
 
-movie_info = {}
-for index, row in enumerate(info_rows):
-    if index == 0:
-        movie_info['Title'] = row.find('th').get_text(" ", strip=True)
-    elif index == 1:
-        continue
-    else:
-        content_key = row.find('th').get_text(" ", strip=True)
-        content_value = get_content_value(row.find('td'))
-        movie_info[content_key] = content_value
-
-print(movie_info)
+if __name__ == "__main__":
+    main()
